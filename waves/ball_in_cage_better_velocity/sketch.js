7bba7432e6115104b07668cfa34c4d82df87bf10
Nihar <niharmadhavan@gmail.com>
2016-03-24 16:09:27 -0400

    better velocity in ball in cages

--- waves/ball_in_cage_better_velocity/sketch.js
+++ waves/ball_in_cage_better_velocity/sketch.js
@@ -1,7 +1,7 @@
 // Spring tied to center of screen, making sound when it hits the walls.
 
-var sensitivity = 40;
-var bounciness = 0.3;
+var sensitivity = 5;
+var bounciness = 0.2;
 var damping = .01;
 var stiffness = 0;
 var accelerationThreshold = 0;
