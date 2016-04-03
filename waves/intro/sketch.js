922f7ab326fd9c8dcc9bb7d50b4a629b5a2c2cd4
Nihar <niharmadhavan@gmail.com>
2016-04-02 23:44:12 -0400

    trying intro screen

--- waves/intro/sketch.js
+++ waves/intro/sketch.js
@@ -11,7 +11,8 @@ function touchStarted() {
   ellipse(touchX, touchY, 5, 5);
   // prevent default
   return false;
-  stroke('white');
+  fill('white');
+  textSize(20,20);
   text(touchX, 20,20);
   text(ptouchX, 20,100);
 
